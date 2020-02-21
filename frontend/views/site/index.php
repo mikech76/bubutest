fbc22dcbf26c2908af90054324f034eb410f5673
ChechinMI <xtxbyelecard>
2020-02-21 19:15:01 +0700

    init

---
+++ frontend/views/site/index.php
@@ -0,0 +1,22 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Привет!</h1>
+
+        <p class="lead">Демонстрация приложения на Yii2.</p>
+
+        <p><a class="btn btn-lg btn-success" href="region">Страны</a></p>
+    </div>
+
+    <div class="body-content">
+
+
+
+    </div>
+</div>
